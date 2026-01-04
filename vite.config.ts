4bfb82fb294c34c950371ff9a241f8dc0acf31c1
spraywaterpark <spraywaterpark@gmail.com>
2026-01-04 08:46:48 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -5,8 +6,5 @@ export default defineConfig({
   plugins: [react()],
   define: {
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
-  },
-  resolve: {
-    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json']
   }
 });
