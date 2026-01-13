dc6d9c6cf5e286dfd436cc14c2dd11c9fe2f8ab4
spraywaterpark <spraywaterpark@gmail.com>
2026-01-13 08:40:14 +0530

    Update admin_lockers.tsx

--- pages/admin_lockers.tsx
+++ pages/admin_lockers.tsx
@@ -1,1 +1,14 @@
+import React from 'react';
+
+const AdminLockers: React.FC = () => {
+  return (
+    <div className="text-white text-center py-32 space-y-6">
+      <h1 className="text-4xl font-black">Locker & Costume Module</h1>
+      <p className="text-white/60">Module loaded successfully.</p>
+      <p className="text-sm text-white/40">Development starts here.</p>
+    </div>
+  );
+};
+
+export default AdminLockers;
 
